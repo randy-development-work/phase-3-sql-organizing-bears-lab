79f93037d348e1c25aac48dd064acd91b5f21ffd
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-15 18:44:33 +0300

    Completed Assignment.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) 
+VALUES 
+(1, "Mr. Chocolate", 3, "M", "Black", "naughty", 1),
+(2, "Rowdy", 3, "M", "Black", "naughty", 1),
+(3, "Tabitha", 3, "M", "Black", "naughty", 1),
+(4, "Sergeant Brown", 3, "M", "Black", "naughty", 1),
+(5, "Melissa", 3, "F", "Black", "naughty", 1),
+(6, "Grinch", 3, "F", "Black", "naughty", 1),
+(7, "Wendy", 3, "M", "Black", "naughty", 1),
+(8, NULL, 3, "M", "Black", "naughty", 1);
